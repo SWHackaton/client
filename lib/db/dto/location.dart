1537f9fff04cbaf1401a8f9b69d6136cab11c794
Junsu Min <sunsuking@gmail.com>
2022-06-23 01:43:55 +0900

    SQLite 작업 - SQLite3 연결 - GeoLocation DAO, DTO 클래스 생성 - CRUD 생성

---
+++ lib/db/dto/location.dart
@@ -0,0 +1,30 @@
+const String tableGeo = 'geolocations';
+const String columnId = '_id';
+const String columnDateTime = 'dateTime';
+const String columnLat = 'latitude';
+const String columnLon = 'longitude';
+
+class GeoLocation {
+  late int? id;
+  late String dateTime;
+  late double latitude;
+  late double longitude;
+
+  GeoLocation({required this.dateTime,required this.latitude, required this.longitude});
+
+  Map<String, dynamic> toMap() {
+    return {
+      'dateTime': dateTime,
+      'latitude': latitude,
+      'longitude': longitude,
+    };
+  }
+
+  GeoLocation.fromMap(Map<dynamic, dynamic> map) {
+    id = map[columnId];
+    dateTime = map[columnDateTime];
+    latitude = map[columnLat];
+    longitude = map[columnLon];
+  }
+}
+
